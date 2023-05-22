47eefaee649ea5a22a7d81d39e75ea3db69641bd
«Kristina» <kristinka007_92@mail.ru>
2023-05-22 18:53:27 +0300

    Задача 1

--- DZ_6.java
+++ DZ_6.java
@@ -1,10 +1,13 @@
-//import java.util.*;
+// //import java.util.*;
 
 public class DZ_6 {
     public static void main(String[] args) {
-        Notebook notebook1 = new Notebook();
-        notebook1.setName("Acer");
-        System.out.println(notebook1.getName());
+        Notebook notebook1 = new Notebook("Acer", 250, "Windows", "red");
+        Notebook notebook2 = new Notebook("Asus", 450, "Linux", "grey");
+        Notebook notebook3 = new Notebook("Mac", 800, "macOS", "white");
+        System.out.println(notebook1);
+        System.out.println(notebook2);
+        System.out.println(notebook3);
     }
 
     
